77af07bbf696d97b8ab5ad89cd8fc497f83b1ae7
berreca16 <146955956+berreca16@users.noreply.github.com>
2024-08-02 09:28:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+const perguntas = [//abre a lista de ojetos (iten)
+    {//abre o item
+        enunciado:"Perguntas 1",
+        alternativas:[
+            "Alternativa 1",
+            'Aternativa 2'
+        ]
+        ]
+
+    }
