44c0cc32de96509228e464fa58fa8ee3ebc196e6
berreca16 <146955956+berreca16@users.noreply.github.com>
2024-08-23 09:25:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
-const caixaAternativa = document.querySelector('.caixa-alternativa');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
-const textoResultado = document.querySelector('.texto-resultado');
-const perguntas = [//abre a lista de ojetos (iten)
+
+const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
-        enunciado: "Perguntas 1",
+        enunciado: "Pergunta 1",
         alternativas: ["Alternativa 1", "Alternativa 2"]
     },
     {
-        enunciado: "Perguntas 2",
+        enunciado: "Pergunta 2",
         alternativas: ["Alternativa 1", "Alternativa 2"]
     },
     {
-        enunciado: "Perguntas 3",
+        enunciado: "Pergunta 3",
         alternativas: ["Alternativa 1", "Alternativa 2"]
     }
 ]
 let posicao = 0;
-let perguntaatual;
+let perguntaAtual;
 
-function mostrapergunta(){
-
-    }
+function mostraPergunta() {
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta.textContent = perguntaAtual
+}
 
