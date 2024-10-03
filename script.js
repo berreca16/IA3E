65ac66b397505a167582f36d68f6f7b401c6929d
berreca16 <146955956+berreca16@users.noreply.github.com>
2024-10-03 07:47:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,44 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado: "Você gosta de cachorros?",
-        alternativas: ["Sim", "Não"]
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Sim gosto muito"
+        },
+        {
+            texto: "Não",
+            afirmação: " Não gosto"
+        }
+        ]
     },
     {
         enunciado: "Você gosta de ouvir músicas?",
-        alternativas: ["Sim", "Não"]
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Sim, sempre"
+        },
+        {
+            texto: "Não",
+            afirmação: "Não gosto"
+        }
+        ]
     },
     {
         enunciado: "Você sabe desenhar?",
-        alternativas: ["Sim", "Não"]
+        alternativas: [{
+            texto: "Sim",
+            afrimação: "Sim, sou muito boa"
+        },
+        {
+            texto: "Não",
+            afrimação: "Não é muito a minha praia"
+        }
+        ]
     }
 ]
 let posicao = 0;
 let perguntaAtual;
+
 
 function mostraPergunta() {
     perguntaAtual = perguntas[posicao];
@@ -28,7 +53,10 @@ function mostraPergunta() {
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativa) {
         const botaoAternativas = document.createElement("button");
-        botaoAternativas.textContent = alternativa;
+        botaoAternativas.textContent = alternativa.texto;
+        botaoAternativas.addEventListener("click", function () {
+            posicao++;
+        });
         caixaAlternativa.appendChild(botaoAternativas);
     }
 }
